5015e3585741c1bc5e429d14b0b7085ba7c28472
flaviomoceri <moceriflavio@gmail.com>
2021-12-26 18:00:31 +0100

    third commit

--- contracts/NDToken.sol
+++ contracts/NDToken.sol
@@ -1,10 +1,12 @@
+
+
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.3;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract NDToken is ERC20 {
-    constructor() ERC20("Nader Dabit Token", "NDT") {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
         _mint(msg.sender, 100000 * (10 ** 18));
     }
 }
